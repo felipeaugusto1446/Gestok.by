79c39b7b790606e549852bd26c06c3b8791f7120
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2025-09-02 16:16:22 -0300

    feat: adiciona configuração inicial do Prisma e middleware de tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { router } from './routes'
 // Importa o Express para criar o servidor
 // Importa o roteador definido no arquivo routes.ts
@@ -15,7 +15,29 @@ app.use(express.json());
 app.use(router);
 // Adiciona as rotas definidas em routes.ts ao servidor
 
-app.listen(port,()=>{
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    // Esse é o middleware de tratamento de erros.
+    // Ele só é chamado quando alguma rota ou middleware anterior lança uma exceção
+    // ou passa um erro para o "next(err)".
+
+    if (err instanceof Error) {
+        // Verifica se o erro é uma instância da classe Error
+        // (ou seja, um erro comum lançado no código).
+        // Nesse caso, retorna status 400 (Bad Request).
+        return response.status(400).json({
+            error: err.message, // Mostra a mensagem de erro para o cliente
+        });
+    }
+    // Se não for uma instância de Error, significa que o problema não foi tratado diretamente.
+    // Aqui é um "catch all" para qualquer erro inesperado.
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error.' // Mensagem genérica para não expor detalhes
+    })
+})
+
+app.listen(port, () => {
     console.log("Servidor rodando na porta 3333 - Projeto Controle de Estoque curso NodeJs")
 });
 // Inicia o servidor na porta configurada e exibe uma mensagem no console
